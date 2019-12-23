f111b5bbd6259e22719198f5672f44a4d7a3d8c7
rankhep <ehdrms1507@naver.com>
2019-12-23 14:20:34 +0900

    Main

---
+++ app/src/main/java/com/example/toss/BorrowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.toss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BorrowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_borrow)
+    }
+}
